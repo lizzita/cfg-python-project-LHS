b75c1a05cfbc78e5550f69e3aab751e47f18b9af
Liz <b8043594@my.shu.ac.uk>
2019-03-27 18:36:09 +0000

    27 March 2019 changes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -18,4 +20,7 @@ def get_feedback():
 
   return render_template("feedback.html", form_data=data)
 
-app.run(debug=True)
+if 'PORT' in os.environ:
+     app.run(host='0.0.0.0', port=int(os.environ['PORT']))
+else:
+     app.run(debug=True)
